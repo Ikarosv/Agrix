a9d394c53f62c6c225efa1a288997842ba9439cb
Ikarosv <ikarovieiraa@gmail.com>
2023-08-22 00:33:37 -0300

    style: adiciona javadoc

--- src/main/java/com/betrybe/agrix/dtos/FarmsDto.java
+++ src/main/java/com/betrybe/agrix/dtos/FarmsDto.java
@@ -2,7 +2,19 @@ package com.betrybe.agrix.dtos;
 
 import com.betrybe.agrix.models.entities.Farm;
 
+/**
+ * Farms DTO.
+ *
+ * @param id id.
+ * @param name name.
+ * @param size size.
+ */
 public record FarmsDto(Integer id, String name, Double size) {
+  /**
+   * Change to farm.
+   *
+   * @return The farm.
+   */
   public Farm toFarm() {
     return new Farm(id, name, size);
   }
--- src/main/java/com/betrybe/agrix/services/FarmsService.java
+++ src/main/java/com/betrybe/agrix/services/FarmsService.java
@@ -5,15 +5,29 @@ import com.betrybe.agrix.models.repositories.FarmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * This class represents the service for the Farm entity.
+ */
 @Service
 public class FarmsService {
   private final FarmRepository farmRepository;
 
+  /**
+   * Constructor.
+   *
+   * @param farmRepository Farm repository.
+   */
   @Autowired
   public FarmsService(FarmRepository farmRepository) {
     this.farmRepository = farmRepository;
   }
 
+  /**
+   *  Creates a farm.
+   *
+   * @param farm The farm to be created.
+   * @return The created farm.
+   */
   public Farm createFarm(Farm farm) {
     return farmRepository.save(farm);
   }
